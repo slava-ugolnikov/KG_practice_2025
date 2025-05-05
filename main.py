37318cbead434147b9bb7662a3ed3591298b6e85
Vyacheslav Ugolnikov <144421672+slava-ugolnikov@users.noreply.github.com>
2025-05-05 05:43:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import argparse
-import logging
 from models.llm_model.py import run_llm_ner
 from models.ml_model.py import run_ml_ner
 from dataset.conll_loader import load_all_conll_sets
+import argparse
+import logging
+
 
 logging.basicConfig(filename='results.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
