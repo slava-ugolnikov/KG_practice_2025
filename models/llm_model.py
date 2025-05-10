7c4c1ffdc2c617d33f429e706692efc78b671004
Vyacheslav Ugolnikov <144421672+slava-ugolnikov@users.noreply.github.com>
2025-05-10 20:19:00 +0300

    Update llm_model.py

--- models/llm_model.py
+++ models/llm_model.py
@@ -63,18 +63,18 @@ def run_llm_ner(conll_data, df_conll, batch_size=20):
         print(sentences)
         sentence_ids = [id for id, text in batch]
         print(sentence_ids)
-    #     try:
-    #         raw_output = ner_llama(sentences, sentence_ids)
-    #         df_llm_batch = parse_llm_output(raw_output)
-    #         all_dfs.append(df_llm_batch)
-    #     except Exception as e:
-    #         print(f"[!] Ошибка в батче {i // batch_size}: {e}")
-    #         continue
+        try:
+            raw_output = ner_llama(sentences, sentence_ids)
+            df_llm_batch = parse_llm_output(raw_output)
+            all_dfs.append(df_llm_batch)
+        except Exception as e:
+            print(f"[!] Ошибка в батче {i // batch_size}: {e}")
+            continue
 
-    # df_llm = pd.concat(all_dfs, ignore_index=True)
-    # print(df_llm.Label_LLM.unique())
-    # precision, recall, f1 = evaluate(df_conll, df_llm)
-    # print(f"Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}")
+    df_llm = pd.concat(all_dfs, ignore_index=True)
+    print(df_llm.Label_LLM.unique())
+    precision, recall, f1 = evaluate(df_conll, df_llm)
+    print(f"Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}")
 
 
 
